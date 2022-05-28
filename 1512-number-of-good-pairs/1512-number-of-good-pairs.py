b68d9c64eeabd8215e68e308ad1626a1868533ca
thumamah <67235392+thumamah@users.noreply.github.com>
2022-05-28 18:14:37 +0100

    Time: 75 ms (5.14%), Space: 13.8 MB (55.31%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,13 +1,25 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         
+        hashMap = {}
+        goodPairs = 0
+        
+        for i in nums:
+            if i in hashMap:
+                goodPairs += hashMap[i]
+                hashMap[i] += 1 
+            else:
+                hashMap[i] = 1
+        return goodPairs
+        
+        
     # Brute Force approach
     # O(n^2) time | O(1) space
     
-        goodPairs = 0
-        for i in range(len(nums)):
-            for j in range(1, len(nums)):
-                if nums[i] == nums[j] and i < j:
-                    goodPairs += 1
-        return goodPairs
+        # goodPairs = 0
+        # for i in range(len(nums)):
+        #     for j in range(1, len(nums)):
+        #         if nums[i] == nums[j] and i < j:
+        #             goodPairs += 1
+        # return goodPairs
         
