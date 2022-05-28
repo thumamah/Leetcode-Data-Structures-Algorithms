82f1579076d2c9348bde3d3b5fddba575021a225
thumamah <67235392+thumamah@users.noreply.github.com>
2022-05-28 15:55:40 +0100

    Time: 120 ms (9.37%), Space: 14.1 MB (40.79%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,10 +1,16 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         
+    
+    # Brute Force approach
+    # O(n) time | O(n) space
+    
         result = []
-        
+        # iterate over nums n times
         for i in range(n):
+            # add element from start(X elements)
             result.append(nums[i])
+            # add element from the second half (Y elements)
             result.append(nums[n+i]) 
             
                  
