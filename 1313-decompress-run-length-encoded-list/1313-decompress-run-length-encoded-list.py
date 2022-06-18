6d5bdba895e44a8d0333e5018b4c413dc06365ac
thumamah <67235392+thumamah@users.noreply.github.com>
2022-06-18 13:26:47 +0100

    Time: 75 ms (78.10%), Space: 14.3 MB (77.88%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
@@ -1,24 +1,33 @@
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
+        
+    # Approach 1
+    # O(nm) time | O(n) space
         output = []
+        # Looping through the nums for n-1 times with step size of 2.
         for i in range(0,len(nums)-1,2):
+            #Frequency element is nums[i] whereas value is nums[i+1], 
             freq = nums[i]
             value = nums[i+1]
+            # again loop through frequency and append the value to the output.
             for j in range(freq):
-                output.append(value)
-                
+                output.append(value)      
         return output
         
+    # Approach 2
+    # O(n) time | O(n) space
 #         decompressed = []
-        
 #         for i in range(0, len(nums)-1, 2):
+        # decompress, then append the number that expanded based on frequency to the list in that order.
+        # Don’t use append, it would actually just multiply the number. By doing what I did here,
+        # you would multiply the number as a list object by the frequency.
 #             decompressed += nums[i] * [nums[i+1]]
-#             print(nums[i] * [nums[i+1]])
-#             print(decompressed)
-    
 #         return decompressed
         
-        
+    
+    
+    
+        # some other technique's for the above problem but not all test cases pass.
 #         result = []
 #         maps = {}
 #         for i in range(0, len(nums), 2): 
