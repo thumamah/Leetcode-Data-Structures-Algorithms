771319ed9b0cdedb513126da5bd8c904c953498f
thumamah <67235392+thumamah@users.noreply.github.com>
2022-06-20 17:02:03 +0100

    Time: 97 ms (30.18%), Space: 13.9 MB (15.08%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,21 +1,33 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         
-    # Approach 1
-    # O(n log(n)) time | O(n) space
-        result = ""
-        # map to store string char as value and index as keys
-        maps = {}
-        # iterate over string
-        for index, char in enumerate(s):
-            #if char is not in map
-            if char not in maps:
-                # then add to map as key being the index and value being the character
-                maps[indices[index]] = char
-        # sort the array to access the index in ascending order
-        indices.sort()
-        # iterate over the sorted list
-        for i in range(len(indices)):
-            # for each index in indixes, add to string according to the index we see in map.
-            result += maps[indices[i]]
-        return result
+        
+    # # Approach 1
+    # # O(n log(n)) time | O(n) space
+    #     result = ""
+    #     # map to store string char as value and index as keys
+    #     maps = {}
+    #     # iterate over string
+    #     for index, char in enumerate(s):
+    #         #if char is not in map
+    #         if char not in maps:
+    #             # then add to map as key being the index and value being the character
+    #             maps[indices[index]] = char
+    #     # sort the array to access the index in ascending order
+    #     indices.sort()
+    #     # iterate over the sorted list
+    #     for i in range(len(indices)):
+    #         # for each index in indixes, add to string according to the index we see in map.
+    #         result += maps[indices[i]]
+    #     return result
+    
+# Approach 2
+# O(n) time | O(n) space
+    
+        ans = [""] * len(indices)
+
+        for i in range(0,len(indices)):
+            
+            ans[indices[i]] = s[i]
+
+        return "".join(ans)
