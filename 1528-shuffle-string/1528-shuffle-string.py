deeda595cb67b3a378b9430670610a8d78e50ef3
thumamah <67235392+thumamah@users.noreply.github.com>
2022-06-20 16:52:53 +0100

    Time: 84 ms (47.65%), Space: 13.8 MB (61.77%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,12 +1,21 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
+        
+    # Approach 1
+    # O(n log(n)) time | O(n) space
         result = ""
+        # map to store string char as value and index as keys
         maps = {}
+        # iterate over string
         for index, char in enumerate(s):
+            #if char is not in map
             if char not in maps:
+                # then add to map as key being the index and value being the character
                 maps[indices[index]] = char
-                print(maps)
+        # sort the array to access the index in ascending order
         indices.sort()
+        # iterate over the sorted list
         for i in range(len(indices)):
+            # for each index in indixes, add to string according to the index we see in map.
             result += maps[indices[i]]
         return result
